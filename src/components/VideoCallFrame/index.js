2b7eeb84fa83c78d43690724ee46f943a97eb5f6
Abibibi <abeba.ngwe@gmail.com>
2020-03-24 06:25:23 +0100

    displaying a confirm box when trying to leave page (works better when trying to change history than closing window / tab though

--- src/components/VideoCallFrame/index.js
+++ src/components/VideoCallFrame/index.js
@@ -1,10 +1,20 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import DailyIframe from '@daily-co/daily-js'
 import './style.css'
+import LeaveModal from './LeaveModal'
 
 const VideoCallFrame = ({ url }) => {
 
     const videoFrame = useRef(null)
+
+    // to display confirmation message 
+    // when user attempts leaving page
+    const leavingCall = (event) => {
+        // Cancel the event as stated by the standard.
+        event.preventDefault()
+        // Chrome requires returnValue to be set.
+        event.returnValue = ''
+    }
 
     useEffect(() => {
         const daily = DailyIframe.createFrame(
@@ -17,14 +27,24 @@ const VideoCallFrame = ({ url }) => {
                     width: '100%',
                     height: '100%'
                 },
-                lang: 'fr'
+                lang: 'fr',
+                showLeaveButton: true
             }
         )
         daily.join({ url })
+
+        window.addEventListener('beforeunload', leavingCall)
+        // ComponentWillUnmount
+        return () => {
+            window.removeEventListener('beforeunload', leavingCall)
+        }
+       
     }, [])
 
     return (
-        <div ref={videoFrame} />
+        <>
+            <div ref={videoFrame} />
+        </>
     )
 }
 
