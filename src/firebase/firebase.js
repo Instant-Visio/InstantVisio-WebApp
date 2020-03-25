7bff25382169bc449208f872f14a14bc6e34f873
Abibibi <abeba.ngwe@gmail.com>
2020-03-25 03:14:18 +0100

    trying a new value

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
+    projectId: 'instantvisio',
 }
 
 firebase.initializeApp(firebaseConfig)
