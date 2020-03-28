d3a52ccd9e1a88cac74eade1ca7866435d73f44f
Abibibi <abeba.ngwe@gmail.com>
2020-03-28 13:21:02 +0100

    with translation in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,23 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Container} from 'react-bootstrap'
+import HeaderStyled from './Header.js/index.js'
+import { useTranslation } from 'react-i18next'
+import Logo from '../Logo'
 
-import InstantVisioLogo from '../../styles/assets/images/Favicon_InstantVisio_Titre.png'
-import InstantVisioLogoMobile from '../../styles/assets/images/Favicon_InstantVisio_Titre_mobile.png'
-import HeaderStyled from './Header.js'
 
-
-const Header = () => (
-    <HeaderStyled>
-        <Container className="header">
-            <img
-                src={InstantVisioLogo}
-                alt="logo de Instant Visio, représentant un écran de téléphone portable bleu où apparaît un visage sous forme de cercle orange, discutant en visio avec le propriétaire du téléphone, dont le visage apparaît sous la forme d'un cercle aux contours oranges."
-                className="header-desktopLogo"
-            />
-            <img
-                src={InstantVisioLogoMobile}
-                alt="logo de Instant Visio, représentant un écran de téléphone portable bleu où apparaît un visage sous forme de cercle orange, discutant en visio avec le propriétaire du téléphone, dont le visage apparaît sous la forme d'un cercle aux contours oranges."
-                className="header-mobileLogo"
-            />
-            <Container className="header-baseline">
-                <p className="header-baseline-content">Joignez un proche en visio, en un clic, gratuitement.</p>
+const Header = () => {
+    const {t} = useTranslation()
+        return (
+        <HeaderStyled>
+            <Container className="header">
+                <Logo />
+                <Container className="header-baseline">
+                    <p className="header-baseline-content">{t('baseline')}</p>
+                </Container>
             </Container>
-        </Container>
-    </HeaderStyled>
-)
+        </HeaderStyled>
+    )
+}
 
 export default Header
