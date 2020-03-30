b005c1c0d6c352412a12971e08afe541f666fb73
Abibibi <abeba.ngwe@gmail.com>
2020-03-30 02:49:40 +0200

    used markdown version of legal mentions in legales-mentions.json for translation

--- src/pages/LegalMentions/index.js
+++ src/pages/LegalMentions/index.js
@@ -1,22 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-remarkable'
 import markdown from '../../mdx/legal-mentions.md'
 
 import DefaultLayout from '../../layout/Default'
 
 const LegalMentions = () => {
-    const [ content, setContent ] = useState(null)
-
-    useEffect(() => {
-        fetch(markdown).then(res => res.text()).then(text => setContent(text))
-    })
-
     const {t} = useTranslation('legal-mentions')
 
     return (
         <DefaultLayout title={`${t('page-title')} - Instant Visio`}>
-            <ReactMarkdown source={content} t={t} />
+            <ReactMarkdown source={t('page-content')} />
         </DefaultLayout>
     )
 }
