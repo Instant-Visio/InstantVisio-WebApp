9eac76fb1910031fe3202ae1e246901def9c63ea
Abibibi <abeba.ngwe@gmail.com>
2020-03-30 05:55:09 +0200

    added partners Daily.co and OVHcloud in Credits

--- src/pages/Credits/index.js
+++ src/pages/Credits/index.js
@@ -12,24 +12,29 @@ const Credits = () => {
         <DefaultLayout title={`${t('page-title')} - Instant Visio`}>
             <CreditsStyled>
                 <div>
-                    <h3 className="default-smallTitle credits-title">{t('creators')}</h3>
+                    <h3 className="credits-title">{t('creators')}</h3>
                     <p><a className="credits-link" href="https://fr.linkedin.com/in/stephanelucon" target="_blank" rel="noopener noreferrer">Stéphane Luçon</a></p>
                 </div>
                 <div>
-                    <h3 className="default-smallTitle credits-title">{t('developers')}</h3>
+                    <h3 className="credits-title">{t('developers')}</h3>
                     <p><a className="credits-link" href="https://abebangwe.com" target="_blank" rel="noopener noreferrer">Abeba Ngwe</a></p>
                     <p><a className="credits-link" href="https://fr.linkedin.com/in/nicolas-tchouanmou-7029a112" target="_blank" rel="noopener noreferrer">Nicolas Tchouanmou</a></p>
                     <p><a className="credits-link" href="http://twitter.com/hugogresse" target="_blank" rel="noopener noreferrer">Hugo Gresse</a></p>
                     <p><a className="credits-link" href="https://fr.linkedin.com/in/stephanelucon" target="_blank" rel="noopener noreferrer">Stéphane Luçon</a></p>
                 </div>
                 <div>
-                    <h3 className="default-smallTitle credits-title">{t('ux-designers')}</h3>
+                    <h3 className="credits-title">{t('ux-designers')}</h3>
                     <p><a className="credits-link" href="https://fr.linkedin.com/in/sebastienlhoste" target="_blank" rel="noopener noreferrer">Sébastien L'Hoste</a></p>
                     <p><a className="credits-link" href="https://fr.linkedin.com/in/antoinedeglesne" target="_blank" rel="noopener noreferrer">Antoine Deglesne</a></p>
                 </div>
                 <div>
-                    <h3 className="default-smallTitle credits-title">{t('graphic-designers')}</h3>
+                    <h3 className="credits-title">{t('graphic-designers')}</h3>
                     <p><a className="credits-link" href="https://fr.linkedin.com/in/oph%C3%A9lie-strezlec-b40385103" target="_blank" rel="noopener noreferrer">Ophélie Strezlec</a></p>
+                </div>
+                <div>
+                    <h3 className="credits-title">{t('partners')}</h3>
+                    <p><a className="credits-link" href="https://www.daily.co" target="_blank" rel="noopener noreferrer">Daily.co</a></p>
+                    <p><a className="credits-link" href="https://www.ovh.com" target="_blank" rel="noopener noreferrer">OVHcloud</a></p>
                 </div>
             </CreditsStyled>
         </DefaultLayout>
