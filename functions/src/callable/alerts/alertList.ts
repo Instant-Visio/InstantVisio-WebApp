d6927eb9d50a3b14e2bf7d13b7c61865059a9b52
Hugo Gresse <hugo.gresse@gmail.com>
2020-04-13 12:34:12 +0200

    Add alerting using OpsGenie (#112)

---
+++ functions/src/callable/alerts/alertList.ts
@@ -0,0 +1,17 @@
+export const ALERT_ROOM_NOT_CREATED = {
+    message: 'Daily.co room not created',
+    alias: 'dailyco-room-not-created',
+    description:
+        'The Daily.co API did not return a new room when asking for this. This prevent user from using the application.',
+    tags: ['OverwriteQuietHours', 'Critical', 'dailyco'],
+    priority: 'P1',
+}
+
+export const ALERT_OVH_SMS_QUOTA_REACHED = {
+    message: 'OVH SMS Quota reached',
+    alias: 'sms-ovh-quota-reached',
+    description:
+        'We don\'t have any more credit on ovh, sms was not delivered',
+    tags: ['OverwriteQuietHours', 'Critical', 'sms'],
+    priority: 'P1',
+}
