f4e1b696e4a61a24b5f59fc2c55283f4cb1e7cf2
Hugo Gresse <hugo.gresse@gmail.com>
2020-04-15 14:18:44 +0200

    Remove height 100% on body & app and change videoCall height to 100vh

--- src/pages/VideoCall/VideoCall.js
+++ src/pages/VideoCall/VideoCall.js
@@ -4,7 +4,7 @@ import {SCREEN} from '../../styles/theme'
 
 export const CallContainer = styled.div`
     width: 100vw;
-    height: 100%;
+    height: 100vh;
     color: ${({theme}) => theme.color.white};
     background: ${({theme}) => theme.color.black};
 `
