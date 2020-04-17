40fa661dbfbd76e67ef8bf1e03f61a077e095f76
ntchou <joe.lindien22@gmail.com>
2020-04-17 13:30:26 +0200

    alphabetical order

--- src/pages/Credits/index.js
+++ src/pages/Credits/index.js
@@ -4,30 +4,39 @@ import DefaultLayout from '../../layout/Default'
 import CreditsStyled from './Credits'
 import data from './data.json'
 
+const sort = ({name: aName}, {name: bName}) => {
+    const [, aLastname] = aName.split(' ')
+    const [, bLastname] = bName.split(' ')
+    if (bLastname && aLastname){
+        return aLastname.localeCompare(bLastname)
+    }
+    
+    return 0
+}
+
+// eslint-disable-next-line react/prop-types
+const renderName = ({name, link}, index) => {
+    return (
+        <p key={`person-${index}`}>
+            {!link && <span>{name}</span>}
+            {link && <a
+                className="credits-link"
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer">
+                {name}</a>}
+        </p>
+    )
+}
 
 const Credits = () => {
     const { t } = useTranslation('credits')
-
-    // eslint-disable-next-line react/prop-types
-    const renderName = ({name, link}, index) => {
-        return (
-            <p key={`person-${index}`}>
-                {!link && <span>{name}</span>}
-                {link && <a
-                    className="credits-link"
-                    href={link}
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    {name}</a>}
-            </p>
-        )
-    }
     
     const render = ([type, values], index) => {
         return (
             <div key={`credit-type-${index}`}>
                 <h3 className="credits-title">{t(type)}</h3>
-                {values.map(renderName)}
+                {values.sort(sort).map(renderName)}
             </div>
         )
     }
