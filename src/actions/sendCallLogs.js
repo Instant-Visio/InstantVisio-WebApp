aab1825d0e12d59b34e24fbadbea26cb0e0d19ab
Abibibi <abeba.ngwe@gmail.com>
2020-04-24 18:53:05 +0200

    now sending call logs to serserver

--- src/actions/sendCallLogs.js
+++ src/actions/sendCallLogs.js
@@ -1,9 +1,23 @@
-export default (roomName, log) => {
+export default async (roomName, log) => {
 
-    const roomLogInfo = {
-        roomName,
-        log
+    const logWithoutAction = () => {
+        delete log.action
+        return log
     }
 
-    console.log(roomLogInfo)
+    const roomLogInfo = {
+        room: roomName,
+        action: log.action,
+        event: logWithoutAction()
+    }
+
+    try {
+        await fetch(process.env.REACT_APP_LOG_TRACKING, {
+            method: 'POST',
+            body: JSON.stringify(roomLogInfo)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
 }
