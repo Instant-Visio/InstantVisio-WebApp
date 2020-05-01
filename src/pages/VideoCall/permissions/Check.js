87ccdf71975a97c4e4e731d9f01d2f6eb78404bb
Hugo Gresse <hugo.gresse@gmail.com>
2020-05-01 11:36:51 +0200

    Add base components for permission dialog

---
+++ src/pages/VideoCall/permissions/Check.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import Icon from '@mdi/react'
+import { mdiCheckCircle, mdiAlertCircle } from '@mdi/js'
+import styled from 'styled-components'
+import { Spinner } from 'react-bootstrap'
+
+export const CheckItem = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    svg {
+        margin-right: 8px;
+    }
+
+    .spinner {
+        margin-right: 12px;
+        margin-left: 4px;
+        width: 30px;
+        height: 30px;
+    }
+`
+
+export const STATE_WAITING = 0
+export const STATE_GRANTED = 1
+export const STATE_DENIED = 2
+
+const iconSize = 1.5
+const Check = ({ state, i18nKey, i18nInstance }) => {
+    const formatData = () => {
+        switch (state) {
+            default:
+            case STATE_WAITING:
+                return {
+                    icon: (
+                        <Spinner
+                            animation="border"
+                            role="status"
+                            className="spinner"
+                        />
+                    ),
+                    text: i18nInstance(`${i18nKey}.waiting`),
+                }
+            case STATE_GRANTED:
+                return {
+                    icon: (
+                        <Icon
+                            path={mdiCheckCircle}
+                            size={iconSize}
+                            color="green"
+                        />
+                    ),
+                    text: i18nInstance(`${i18nKey}.granted`),
+                }
+            case STATE_DENIED:
+                return {
+                    icon: (
+                        <Icon
+                            path={mdiAlertCircle}
+                            size={iconSize}
+                            color="red"
+                        />
+                    ),
+                    text: i18nInstance(`${i18nKey}.denied`),
+                }
+        }
+    }
+
+    const data = formatData(state)
+
+    return (
+        <CheckItem>
+            {data.icon}
+
+            {data.text}
+        </CheckItem>
+    )
+}
+
+export default Check
