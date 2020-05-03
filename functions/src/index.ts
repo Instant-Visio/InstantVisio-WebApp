413189530fd773880a6fead2443bf2ea3589d897
Hugo Gresse <hugo.gresse@gmail.com>
2020-05-03 10:23:10 +0200

    Remvoe mailInvite ans add base blocking mechanisme to prevent suspicious form submit (#173)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,3 @@
-export { mailInvite } from './http/mailInvite'
 export { newCall } from './callable/newCall'
 export { roomJoined } from './callable/roomJoined'
 export { logCleaner } from './cron/logCleaner'
