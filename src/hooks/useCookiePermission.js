7afe40a4d584cf863661c176cf1861e85e752b45
Hugo Gresse <hugo.gresse@gmail.com>
2020-05-05 18:00:00 +0200

    Unused import

--- src/hooks/useCookiePermission.js
+++ src/hooks/useCookiePermission.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getCookieBot, hasCookiebot } from '../utils/gdpr'
 import {
     STATE_DENIED,
