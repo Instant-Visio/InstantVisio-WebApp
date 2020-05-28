6fdabb058c24446784ffcf356684e390ef600dd2
Mattia Primavera <sconqua@gmail.com>
2020-05-28 04:10:08 +0200

    UI improvements credits & legal mentions

--- src/pages/LegalMentions/index.js
+++ src/pages/LegalMentions/index.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import ReactMarkdown from 'react-remarkable'
-
+import styled from 'styled-components'
 import DefaultLayout from '../../layout/Default'
+import useDetectMobileOrTablet from '../../hooks/useDetectMobileOrTablet'
+
+const MarkdownContainer = styled.div`
+    p {
+        font-size: ${({ theme }) => theme.font.XS};
+    }
+    h1 {
+        font-size: ${({ theme }) => theme.font.M};
+    }
+    h2 {
+        font-size: ${({ theme }) => theme.font.L};
+    }
+    h3 {
+        font-size: ${({ theme }) => theme.font.S};
+    }
+`
 
 const LegalMentions = () => {
     const { t } = useTranslation('legal-mentions')
+    const isMobile = useDetectMobileOrTablet()
 
     return (
         <DefaultLayout title={`${t('page-title')} - Instant Visio`}>
-            <ReactMarkdown source={t('page-content')} />
+            {isMobile ? (
+                <MarkdownContainer>
+                    <ReactMarkdown source={t('page-content')} />
+                </MarkdownContainer>
+            ) : (
+                <ReactMarkdown source={t('page-content')} />
+            )}
         </DefaultLayout>
     )
 }
