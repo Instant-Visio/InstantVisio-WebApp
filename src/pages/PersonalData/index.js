ffcc0e66cb14206989b1aae9c5b7387ee800b65b
Mattia Primavera <sconqua@gmail.com>
2020-05-31 23:47:20 +0200

    Fix vertical scroll
      #238

--- src/pages/PersonalData/index.js
+++ src/pages/PersonalData/index.js
@@ -3,18 +3,15 @@ import { useTranslation } from 'react-i18next'
 import ReactMarkdown from 'react-remarkable'
 
 import DefaultLayout from '../../layout/Default'
-import { IonContent } from '@ionic/react'
 
 const PersonalData = () => {
     const { t } = useTranslation('personal-data')
 
     return (
         <>
-            <IonContent>
-                <DefaultLayout title={`${t('page-title')} - Instant Visio`}>
-                    <ReactMarkdown source={t('page-content')} />
-                </DefaultLayout>
-            </IonContent>
+            <DefaultLayout title={`${t('page-title')} - Instant Visio`}>
+                <ReactMarkdown source={t('page-content')} />
+            </DefaultLayout>
         </>
     )
 }
