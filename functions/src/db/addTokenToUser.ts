a8909ff5d88f3926ba4433ecddfc84dd6ca12ffe
Hugo Gresse <hugo.gresse@gmail.com>
2020-11-11 13:59:51 +0100

    Remove return success for addTokenToUser

--- functions/src/db/addTokenToUser.ts
+++ functions/src/db/addTokenToUser.ts
@@ -5,29 +5,19 @@ import { JWTToken } from '../types/JWT'
 export const addTokenToUser = async (
     userId: UID,
     token: JWTToken
-): Promise<{ success: boolean }> => {
-    try {
-        await db
-            .collection('users')
-            .doc(userId)
-            .set(
-                {
-                    tokens: {
-                        [token]: {
-                            valid: true,
-                            createdAt: serverTimestamp(),
-                        },
+): Promise<void> => {
+    await db
+        .collection('users')
+        .doc(userId)
+        .set(
+            {
+                tokens: {
+                    [token]: {
+                        valid: true,
+                        createdAt: serverTimestamp(),
                     },
                 },
-                { merge: true }
-            )
-
-        return {
-            success: true,
-        }
-    } catch (error) {
-        return {
-            success: false,
-        }
-    }
+            },
+            { merge: true }
+        )
 }
