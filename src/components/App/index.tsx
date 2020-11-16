4692c7d1be030ea76e0761b1eefb084b2b505c5e
Mattia Primavera <sconqua@gmail.com>
2020-11-16 19:53:04 +0100

    Configure Redux DevTools (for chrome)

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -13,8 +13,12 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../../reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
 
 declare global {
     interface Window {
