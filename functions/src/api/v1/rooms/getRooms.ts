3e9047b78efced02f53939a9bc27f3af080879f6
ntchou <joe.lindien22@gmail.com>
2020-11-28 13:00:18 +0100

    Implement api to get all rooms (#293)
    * Api get all rooms (#291)
    - update swagger doc
    
    * feebacks
    - order by createdAt
    - add index to firestore.indexes.json

--- functions/src/api/v1/rooms/getRooms.ts
+++ functions/src/api/v1/rooms/getRooms.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { getRooms as getRoomsList } from '../../../db/getRooms'
 
 /**
  * @swagger
@@ -6,9 +7,22 @@ import { Request, Response } from 'express'
  *   get:
  *     description: List created rooms.
  *     tags: ['rooms']
+ *     produces:
+ *       - application/json
  *     responses:
  *       200:
  *         description: All the rooms linked to this token
+ *         content:
+ *           application/json:
+ *             example: [{
+ *               id: 'vXIUuaGkH4kukbwRH5cU',
+ *               createdAt: 1605969562,
+ *               updatedAt: 1605969562
+ *             }]
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Room'
  *       401:
  *         description: missing authorization bearer token
  *       403:
@@ -16,11 +30,9 @@ import { Request, Response } from 'express'
  *       412:
  *         description: authorization header present but not formatted correctly
  */
-export const getRooms = async (req: Request, res: Response) => {
-    try {
-        res.send('Hello world')
-    } catch (error) {
-        console.error(error) // eslint-disable-line no-console
-        res.status(500).end()
-    }
+export const getRooms = async (_: Request, res: Response) => {
+    const uid = res.locals.uid
+    const rooms = await getRoomsList(uid)
+
+    res.send(rooms)
 }
