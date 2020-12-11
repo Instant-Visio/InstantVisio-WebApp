bf8ae9d945b12310039faadd06703e00a739cb45
Mattia Primavera <sconqua@gmail.com>
2020-12-11 17:11:29 +0100

    Add linter rules + lint npm script (#306)
    - react-hooks
    - prefer-destructuring
    - require-await
    - no-return-await
    
    Co-authored-by: Hugo Gresse <hugo.gresse@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,32 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: 'module',
     },
-    plugins: ['react'],
+    plugins: ['react', 'react-hooks'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
     rules: {
-        indent: ['error', 4],
+        indent: ['error', 4, { ignoredNodes: ['TemplateLiteral'] }], // https://github.com/babel/babel-eslint/issues/681#issuecomment-451336031
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'never'],
+        'prefer-destructuring': [
+            'error',
+            {
+                array: true,
+                object: true,
+            },
+            {
+                enforceForRenamedProperties: false,
+            },
+        ],
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'error',
+        'no-empty': ['error', { allowEmptyCatch: false }],
+        'require-await': 'error',
+        'no-return-await': 'error',
     },
     overrides: [
         {
