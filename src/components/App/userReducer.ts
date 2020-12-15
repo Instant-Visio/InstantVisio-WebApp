b9a5ec9cdc85a9b7575ef9c77a8eebd3938de0a1
ntchou <joe.lindien22@gmail.com>
2020-12-15 22:02:24 +0100

    using immer

--- src/components/App/userReducer.ts
+++ src/components/App/userReducer.ts
@@ -1,4 +1,4 @@
-import { JWTToken } from '../../../types/JWT'
+/*import { JWTToken } from '../../../types/JWT'
 import { UID } from '../../../types/uid'
 import { SET_TOKEN } from '../../actions/types'
 
@@ -27,4 +27,48 @@ export const userReducer = (
         default:
             return state
     }
+}*/
+
+export interface User {
+    token: JWTToken
+    userId: UID
+    isAnonymous: boolean
 }
+
+import { UID } from '../../../types/uid'
+import { SIGNIN_SUCCESS } from '../../actions/userActionsTypes'
+import { JWTToken } from '../../../types/JWT'
+import { SIGNOUT, SIGNIN_ERROR } from '../../actions/userActionsTypes'
+import produce from 'immer'
+
+export interface UserState {
+    user: User
+    isLoading: boolean
+    error: string | null
+}
+
+const initialState = {
+    user: {
+        token: '',
+        userId: '',
+    },
+    isLoading: true,
+    error: null,
+}
+
+export const userReducer = produce((draft, { type, payload }) => {
+    switch (type) {
+        case SIGNIN_SUCCESS:
+            /*draft.token = payload.token
+            draft.isAnonymous = payload.isAnonymous
+            draft.isLoading = false*/
+            break
+        case SIGNIN_ERROR:
+        /*draft.error = payload.error
+            draft.isLoading = false*/
+        case SIGNOUT:
+            /*draft.token = null
+            draft.isLoading = false*/
+            break
+    }
+}, initialState)
