797f3e6dd257d8af99672742fb8e4a24cd6cac9c
Nicolas Hovart <hovart.nicolas@gmail.com>
2020-12-18 13:37:31 +0100

    Update CallNotLaunched.tsx

--- src/pages/WelcomeCall/CallNotLaunched.tsx
+++ src/pages/WelcomeCall/CallNotLaunched.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { IonContent } from '@ionic/react'
-import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
-import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import SvgIcon from '@material-ui/core/SvgIcon'
+import { Link } from 'react-router-dom'
 
 const InformationBlock = styled.div`
     height: 100%;
@@ -18,27 +18,58 @@ const InformationBlock = styled.div`
     h2 {
         font-weight: 'bold';
     }
-
     h3:nth-of-type(2) {
-        margin-top: 2rem;
+        margin-bottom: 4rem;
+    }
+    h4 {
         margin-bottom: 2rem;
     }
+    a {
+        color: black;
+        text-decoration: underline;
+    }
 `
+const CenteredText = styled.div`
+    text-align: center;
+`
+const MenuList = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+function AccessTimeIcon(props) {
+    return (
+        <SvgIcon {...props}>
+            <path
+                fill="currentColor"
+                d="M12 20C16.4 20 20 16.4 20 12S16.4 4 12 4 4 7.6 4 12 7.6 20 12 20M12 2C17.5 2 22 6.5 22 12S17.5 22 12 22C6.5 22 2 17.5 2 12C2 6.5 6.5 2 12 2M17 11.5V13H11V7H12.5V11.5H17Z"
+            />
+        </SvgIcon>
+    )
+}
 
 export default function CallNotLaunched() {
     const { t } = useTranslation('call-not-launched')
 
     return (
         <IonContent style={{ '--background': 'white' }}>
-            <InformationBlock>
-                <AccessTimeIcon fontSize="large" />
-                <h2>{t('discussion-is-not-launched')}</h2>
-                <h3>{t('takes-places-at')}</h3>
-                <h3>{t('too-early')}</h3>
-                <h4>{t('enjoy')}</h4>
-                <Button>{t('discover-join')}</Button>
-                <Button>{t('discover-create')}</Button>
-                <Button>{t('discover-us')}</Button>
+            <InformationBlock title="not-used-here">
+                <CenteredText>
+                    <AccessTimeIcon style={{ fontSize: 110 }} />
+                    <h2>{t('discussion-is-not-launched')}</h2>
+                    <h3>{t('takes-places-at')}</h3>
+                    <h3>{t('too-early')}</h3>
+                    <h4>{t('enjoy')}</h4>
+                    <MenuList>
+                        <Link to="/pages/join-last-call">
+                            {t('discover-join')}
+                        </Link>
+                        <Link to="/pages/home">{t('discover-create')}</Link>
+                        <Link to="/static/media/Instant_Visio_Keynote.e243b3bd.pdf">
+                            {t('discover-us')}
+                        </Link>
+                    </MenuList>
+                </CenteredText>
             </InformationBlock>
         </IonContent>
     )
