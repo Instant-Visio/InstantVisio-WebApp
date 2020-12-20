3362dcd14e3021ddf41235f4a2981e7c75fb20fa
DOMINIQUE BERNARD KANGA <kangadominique123@gmail.com>
2020-12-20 13:54:51 +0100

    Update callRating.ts
    Remove empty

--- functions/src/callable/callRating.ts
+++ functions/src/callable/callRating.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions'
 import { logCallRating } from '../sumologic/sumologic'
-import { isEmpty } from 'lodash'
 
 export const callRating = functions.https.onCall((data) => {
     if (!Number.isInteger(data?.rating)) {
