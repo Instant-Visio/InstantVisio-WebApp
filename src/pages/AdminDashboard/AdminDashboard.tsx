60a2241295ab6dc9247b88e6423dc4f51a4e9497
Mattia Primavera <sconqua@gmail.com>
2020-12-26 18:12:28 +0100

    Admin dashboard + Invite participants enhancement (#412)
    * Wip for 328 + 341.
    
    Things to do later :
    
    - remove admin route in App/router.tsx
    - manage css in App/App.scss to not have conflict with MuiTheme
    - Manage Login from App/index.tsx (for the moment it is displayed over other screens)
    
    * Prettify admin dashboard
    
    * Uncomment general app styling
    
    * White background for admin dashboard
    
    * Prettify CreateRoomForm
    
    * Add Dashboard & form translations
    
    Co-authored-by: Hugo Tembremande <htembremande@ineat.fr>

---
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -0,0 +1,156 @@
+import React from 'react'
+import { IonContent } from '@ionic/react'
+import Typography from '@material-ui/core/Typography'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from '../../styles/muiTheme'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import Paper from '@material-ui/core/Paper'
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline'
+import RestaurantIcon from '@material-ui/icons/Restaurant'
+import FingerprintIcon from '@material-ui/icons/Fingerprint'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Link from '@material-ui/core/Link'
+import CreateRoomForm from './CreateRoomForm'
+import { useTranslation } from 'react-i18next'
+
+const AdminDashboard = () => {
+    const { t } = useTranslation('dashboard')
+    const preventDefault = (event: React.SyntheticEvent) =>
+        event.preventDefault()
+
+    const useStyles = makeStyles((theme: Theme) =>
+        createStyles({
+            icon: {
+                marginLeft: theme.spacing(2),
+            },
+            textRight: {
+                textAlign: 'right',
+            },
+            list: {
+                paddingTop: 0,
+                paddingBottom: 0,
+            },
+            listItem: {
+                paddingTop: 0,
+                paddingBottom: 0,
+            },
+        })
+    )
+
+    const classes = useStyles()
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <IonContent style={{ '--background': 'white' }}>
+                <Container maxWidth="lg">
+                    <Box m={6} />
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} sm={6}>
+                            <CreateRoomForm />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Paper elevation={0}>
+                                <Grid container alignItems="center" spacing={2}>
+                                    <Grid item>
+                                        <ChatBubbleOutlineIcon
+                                            className={classes.icon}
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="h6">
+                                            {t('planned-discussions')}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Divider />
+                                <List component="nav" className={classes.list}>
+                                    <ListItem className={classes.listItem}>
+                                        <ListItemText primary="Comité de pilotage" />
+                                        <ListItemText
+                                            primary="30 mai"
+                                            primaryTypographyProps={{
+                                                align: 'right',
+                                            }}
+                                        />
+                                    </ListItem>
+                                    <Divider variant="middle" />
+                                    <ListItem className={classes.list}>
+                                        <ListItemText primary="Comité de pilotage" />
+                                        <ListItemText
+                                            primary="30 mai"
+                                            primaryTypographyProps={{
+                                                align: 'right',
+                                            }}
+                                        />
+                                    </ListItem>
+                                </List>
+                            </Paper>
+                            <Box m={6} />
+                            <Paper elevation={0}>
+                                <Grid container alignItems="center" spacing={2}>
+                                    <Grid item>
+                                        <RestaurantIcon
+                                            className={classes.icon}
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="h6">
+                                            {t('consumption')}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Divider />
+                                <ListItem className={classes.list}>
+                                    <ListItemText
+                                        primary={`250 SMS ${t('consumed')} ${t(
+                                            'on'
+                                        )} 500 ${t('available')}`}
+                                    />
+                                </ListItem>
+                            </Paper>
+
+                            <Box m={6} />
+
+                            <Paper elevation={0}>
+                                <Grid container alignItems="center" spacing={2}>
+                                    <Grid item>
+                                        <FingerprintIcon
+                                            className={classes.icon}
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="h6">
+                                            {t('api-identifier')}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs className={classes.textRight}>
+                                        <Link
+                                            href="#"
+                                            onClick={preventDefault}
+                                            variant="body2">
+                                            {t('click-to-copy')}
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                                <Divider />
+                                <ListItem className={classes.list}>
+                                    <ListItemText primary="fjqdkjvq7gjhqdjfd9fsdf767" />
+                                </ListItem>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </IonContent>
+        </ThemeProvider>
+    )
+}
+
+export default AdminDashboard
---
+++ src/pages/AdminDashboard/CreateRoomForm.tsx
@@ -0,0 +1,273 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import { TextField } from 'formik-material-ui'
+import MuiTextField from '@material-ui/core/TextField'
+import {
+    Autocomplete,
+    AutocompleteRenderInputParams,
+} from 'formik-material-ui-lab'
+import { spacing } from '@material-ui/system'
+import { styled } from '@material-ui/core/styles'
+import MuiButton from '@material-ui/core/Button'
+import { Formik, Form, Field } from 'formik'
+import CheckBoxIcon from '@material-ui/icons/CheckBox'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
+import frLocale from 'date-fns/locale/fr'
+import { DateTimePicker } from 'formik-material-ui-pickers'
+import Chip from '@material-ui/core/Chip'
+import Divider from '@material-ui/core/Divider'
+import Grid from '@material-ui/core/Grid'
+import {
+    makeStyles,
+    Theme,
+    createStyles,
+    withStyles,
+} from '@material-ui/core/styles'
+import Flags from 'country-flag-icons/react/3x2'
+import { parsePhoneNumber } from 'libphonenumber-js'
+import { useTranslation } from 'react-i18next'
+
+const Button = styled(MuiButton)(spacing)
+
+const CreateRoomForm = () => {
+    const { t } = useTranslation('dashboard')
+    interface Values {
+        roomName: string
+        participants: Array<any>
+        startAt: Date
+    }
+
+    const [value, setValue] = React.useState([])
+
+    const ErrorChip = withStyles({
+        root: {
+            borderColor: 'red',
+        },
+    })(Chip)
+
+    const useStyles = makeStyles((theme: Theme) =>
+        createStyles({
+            chip: {
+                borderColor: 'green',
+            },
+            chipIconContainer: {
+                width: 'fit-content',
+            },
+            flagIcon: {
+                height: '0.8rem',
+                marginRight: theme.spacing(1),
+                marginLeft: theme.spacing(1),
+            },
+        })
+    )
+
+    const classes = useStyles()
+
+    const handleKeyDown = (event) => {
+        switch (event.keyCode) {
+            case 186: // ;
+            case 32: // space
+            case 9: {
+                // tab
+                event.preventDefault()
+                event.stopPropagation()
+                if (event.target.value.length > 0) {
+                    const newValue = [...value, event.target.value] as never[]
+                    setValue(newValue)
+                }
+                break
+            }
+            default:
+        }
+    }
+
+    const validateEmail = (email) => {
+        const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        return regexp.test(email)
+    }
+
+    const validatePhoneNumber = (phoneNumber) => {
+        let isValid = true
+
+        try {
+            parsePhoneNumber(phoneNumber, 'FR')
+        } catch (error) {
+            isValid = false
+        }
+
+        return isValid
+    }
+
+    const isParticipantValid = (participant) => {
+        return validateEmail(participant) || validatePhoneNumber(participant)
+    }
+    interface MyInputProps {
+        onKeyDown: (event: object) => void
+    }
+    interface MyParams extends AutocompleteRenderInputParams {
+        inputProps: MyInputProps
+    }
+
+    const renderFlagIcon = (participant) => {
+        try {
+            const phoneNumber = parsePhoneNumber(participant, 'FR')
+            const FlagIcon = Flags[phoneNumber.country]
+            return (
+                <Grid
+                    container
+                    alignItems="center"
+                    className={classes.chipIconContainer}>
+                    <FlagIcon
+                        title="United States"
+                        className={classes.flagIcon}
+                    />
+                    <Divider orientation="vertical" flexItem />
+                </Grid>
+            )
+        } catch (error) {
+            return null
+        }
+    }
+
+    const renderTag = (participant, index, getTagProps) => {
+        let ChipElement
+
+        if (isParticipantValid(participant)) {
+            ChipElement = Chip
+        } else {
+            ChipElement = ErrorChip
+        }
+
+        return (
+            <ChipElement
+                icon={renderFlagIcon(participant)}
+                variant="outlined"
+                label={participant}
+                size="small"
+                {...getTagProps({ index })}
+            />
+        )
+    }
+
+    return (
+        <Formik
+            initialValues={{
+                roomName: '',
+                participants: [],
+                startAt: null,
+            }}
+            validate={(values) => {
+                const errors: Partial<Values> = {}
+                if (!values.roomName) {
+                    errors.roomName = 'Required'
+                }
+                return errors
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    setSubmitting(false)
+                    alert(JSON.stringify(values, null, 2))
+                }, 500)
+            }}>
+            {({ submitForm, isSubmitting }) => (
+                <MuiPickersUtilsProvider utils={DateFnsUtils} locale={frLocale}>
+                    <Form>
+                        <Typography variant="h5" component="h1">
+                            {t('form.create-visio')}
+                        </Typography>
+                        <Box m={4} />
+                        <Typography variant="h6" component="h2">
+                            {t('form.visio-name.title')}
+                        </Typography>
+                        <Typography variant="body1">
+                            {t('form.visio-name.description')}
+                        </Typography>
+                        <Box m={2} />
+                        <Field
+                            size="small"
+                            component={TextField}
+                            variant="outlined"
+                            name="roomName"
+                            label={t('form.visio-name.placeholder')}
+                        />
+                        <Box m={4} />
+                        <Typography variant="h6" component="h2">
+                            {t('form.participants.title')}
+                        </Typography>
+                        <Typography variant="body1">
+                            {t('form.participants.description')}
+                        </Typography>
+                        <Box m={2} />
+                        <Typography variant="body2">
+                            {t('form.participants.description2')}
+                        </Typography>
+                        <Field
+                            size="small"
+                            name="participants"
+                            component={Autocomplete}
+                            multiple
+                            freeSolo
+                            value={value}
+                            onChange={(event, newValue) => setValue(newValue)}
+                            options={[]}
+                            defaultValue={[]}
+                            renderTags={(value, getTagProps) =>
+                                value.map((participant, index) =>
+                                    renderTag(participant, index, getTagProps)
+                                )
+                            }
+                            renderInput={(params: MyParams) => {
+                                params.inputProps.onKeyDown = handleKeyDown
+                                return (
+                                    <MuiTextField
+                                        {...params}
+                                        error={false}
+                                        helperText={false}
+                                        label={t(
+                                            'form.participants.placeholder'
+                                        )}
+                                        variant="outlined"
+                                    />
+                                )
+                            }}
+                        />
+                        <Box m={4} />
+                        <Typography variant="h6" component="h2">
+                            {t('form.plan-visio.title')}
+                        </Typography>
+                        <Typography variant="body1">
+                            {t('form.plan-visio.description')}
+                        </Typography>
+                        <Box m={2} />
+                        <Field
+                            name="startAt"
+                            size="small"
+                            component={DateTimePicker}
+                            inputVariant="outlined"
+                            placeholder={t('form.date.placeholder')}
+                            fullWidth
+                            locale="fr"
+                            format="dd MMMM yyyy à HH:MM"
+                            ampm={false}
+                        />
+                        <Box m={4} />
+                        <Box display="flex" justifyContent="flex-end">
+                            <Button
+                                startIcon={<CheckBoxIcon />}
+                                variant="contained"
+                                color="primary"
+                                disabled={isSubmitting}
+                                onClick={submitForm}>
+                                {t('form.submit')}
+                            </Button>
+                        </Box>
+                    </Form>
+                </MuiPickersUtilsProvider>
+            )}
+        </Formik>
+    )
+}
+
+export default CreateRoomForm
