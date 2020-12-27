c735d5260ab4964bfc01d581f29b03e0c6828cb5
Mattia Primavera <sconqua@gmail.com>
2020-12-27 12:33:46 +0100

    Convert an anonymous account to a permanent account

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -30,6 +30,20 @@ const uiConfig = {
         },
         signInFailure: async function (error) {
             console.log('Signin failure error: ', error)
+
+            // For merge conflicts, the error.code will be
+            // 'firebaseui/anonymous-upgrade-merge-conflict'.
+            if (error.code !== 'firebaseui/anonymous-upgrade-merge-conflict') {
+                return Promise.resolve()
+            }
+            // The credential the user tried to sign in with.
+            var cred = error.credential
+            // Copy data from anonymous user to permanent user and delete anonymous
+            // user.
+            // ...
+            // Finish sign-in after data is copied.
+            await authInstance.signInWithCredential(cred)
+            return
         },
     },
 }
