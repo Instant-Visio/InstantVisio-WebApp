24061534645308611c6581b15ffd03836fad3e88
Nicolas Hovart <hovart.nicolas@gmail.com>
2021-01-02 19:32:24 +0100

    Update responsive ( 😭) and Add buttons

--- src/pages/Pricing/Pricing.tsx
+++ src/pages/Pricing/Pricing.tsx
@@ -1,68 +1,93 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useTranslation } from 'react-i18next'
-import VerticallyCenteredModal from '../../components/VerticallyCenteredModal/VerticallyCenteredModal'
-import useDetectMobileOrTablet from '../../hooks/useDetectMobileOrTablet'
 import { SCREEN } from '../../styles/theme'
+import Button from '@material-ui/core/Button'
+import { IonContent } from '@ionic/react'
 
 const Wrapper = styled.div`
     display: flex;
-    position: relative;
-    flex-direction: column;
-    ${SCREEN.DESKTOP} {
-        flex-direction: row;
+    flex-direction: row;
+    justify-content: space-evenly;
+    ${SCREEN.MOBILE} {
+        flex-direction: column;
     }
 `
 
 const ColumnView = styled.div`
-    padding: ${({ theme }) => `4rem ${theme.spacing.L} ${theme.spacing.L}`};
-    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: ${({ theme }) =>
+        `4rem ${theme.spacing.XXXL} 4rem ${theme.spacing.XXXL}`};
     background: ${({ theme }) => theme.color.white};
-    border: 1px solid black;
-    margin: ${({ theme }) => theme.spacing.L};
+    border: 1px solid #c3cfd9;
+    margin: ${({ theme }) => theme.spacing.XXL};
+
+    h3 {
+        margin-bottom: ${({ theme }) => theme.spacing.XXXL};
+    }
+
+    h5:nth-of-type(1) {
+        height: 7rem;
+    }
+    p {
+        margin-bottom: 0rem;
+    }
+
+    // TODO: customize color of Mui-button with color='#6558f5'
+    button {
+        align-self: center;
+        margin-top: ${({ theme }) => theme.spacing.XL};
+    }
+
+    ${SCREEN.MOBILE} {
+        flex-direction: column;
+        padding: ${({ theme }) =>
+            `${theme.spacing.XXL} 3rem ${theme.spacing.XXL} 3rem`};
+    }
     ${SCREEN.DESKTOP} {
         padding: ${({ theme }) =>
-            `${theme.spacing.XXXL} ${theme.spacing.M} ${theme.spacing.XXXL} ${theme.spacing.M}`};
-        width: 55%;
+            `${theme.spacing.XXXL} 3rem ${theme.spacing.XXXL} 3rem`};
+        width: 30%;
     }
-`
-
-const WrapperMobile = styled.div``
-
-const MobileContent = styled.div`
-    padding-left: 5%;
-    padding-right: 5%;
-    color: ${({ theme }) => theme.color.white};
 `
 
 export default function Pricing() {
     const { t } = useTranslation('pricing')
-    const isMobile = useDetectMobileOrTablet()
-    const [modalShow, setModalShow] = React.useState(false)
 
     return (
-        <>
-            {!isMobile ? (
-                <Wrapper>
-                    <ColumnView>
-                        <h2>{t('instant-visio')}</h2>
-                    </ColumnView>
-                    <ColumnView>
-                        <h2>{t('entreprise-institution')}</h2>
-                    </ColumnView>
-                </Wrapper>
-            ) : (
-                <WrapperMobile key={'anchor-left'}>
-                    <MobileContent>
-                        <VerticallyCenteredModal
-                            show={modalShow}
-                            onHide={() =>
-                                setModalShow(false)
-                            }></VerticallyCenteredModal>
-                        Version Mobile
-                    </MobileContent>
-                </WrapperMobile>
-            )}
-        </>
+        <IonContent style={{ '--background': 'white' }}>
+            <Wrapper>
+                <ColumnView>
+                    <div className="textContainer">
+                        <h3>{t('instant-visio')}</h3>
+                        <h5>{t('item0')}</h5>
+                        <p>✔ {t('item1')}</p>
+                        <p>✔ {t('item2')}</p>
+                        <p>✔ {t('item3')}</p>
+                    </div>
+                    <Button variant="contained" color="primary">
+                        {t('support-us')}
+                    </Button>
+                </ColumnView>
+                <ColumnView>
+                    <div className="textContainer">
+                        <h3>{t('entreprise-institution')}</h3>
+                        <h5>{t('item4')}</h5>
+                        <p>✔ {t('item5')}</p>
+                        <p>✔ {t('item6')}</p>
+                        <p>✔ {t('item7')}</p>
+                        <p>✔ {t('item8')}</p>
+                        <p>✔ {t('item9')}</p>
+                        <p>✔ {t('item10')}</p>
+                        <p>✔ {t('item11')}</p>
+                    </div>
+                    <Button variant="contained" color="primary">
+                        {t('choose-subscription')}
+                    </Button>
+                </ColumnView>
+            </Wrapper>
+        </IonContent>
     )
 }
