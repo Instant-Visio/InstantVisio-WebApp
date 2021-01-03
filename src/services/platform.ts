9fec9d8b869aec814d109372458d8a7f323dfaf4
Kanga DOMINIQUE BERNARD <bkangadominique@pca.ma>
2021-01-03 16:58:01 +0100

    fix Unexpected whitespace before property indexOf  no-whitespace-before-property

--- src/services/platform.ts
+++ src/services/platform.ts
@@ -4,7 +4,7 @@ export const isIos = () => isPlatform('ios')
 
 export const isAndroid = () => isPlatform('android')
 
-export const isAndroidAndWeb = () => !getPlatforms().some(r=>["ios","ipad","iphone","phablet","tablet","cordova","capacitor","electron","pwa" ,"mobile","mobileweb","hybrid"] .indexOf(r) >= 0)
+export const isAndroidAndWeb = () => !getPlatforms().some(r=>["ios","ipad","iphone","phablet","tablet","cordova","capacitor","electron","pwa" ,"mobile","mobileweb","hybrid"].indexOf(r) >= 0);
 
 export const isMobile = () => isIos() || isAndroid()
 export const isWeb = () => !isMobile()
