f3fdbb78f50b65100e9255a509b42de622c3937b
Mattia Primavera <sconqua@gmail.com>
2021-01-04 00:13:31 +0100

    Show join btn on Native platforms only

--- src/services/platform.ts
+++ src/services/platform.ts
@@ -4,7 +4,10 @@ export const isIos = () => isPlatform('ios')
 
 export const isAndroid = () => isPlatform('android')
 
-export const isAndroidAndWeb = () => !getPlatforms().some(r=>["desktop","electron","pwa"].indexOf(r) >= 0);
+export const isNative = () =>
+    !getPlatforms().some(
+        (r) => ['desktop', 'electron', 'pwa', 'mobileweb'].indexOf(r) >= 0
+    )
 
 export const isMobile = () => isIos() || isAndroid()
 export const isWeb = () => !isMobile()
