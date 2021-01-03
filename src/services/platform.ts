2d0f91e0b8ca34e6d4ff03c7a3fc37743fa25ced
Kanga DOMINIQUE BERNARD <bkangadominique@pca.ma>
2021-01-03 16:34:53 +0100

    #514

--- src/services/platform.ts
+++ src/services/platform.ts
@@ -1,8 +1,10 @@
-import { isPlatform } from '@ionic/react'
+import { isPlatform,getPlatforms } from '@ionic/react'
 
 export const isIos = () => isPlatform('ios')
 
 export const isAndroid = () => isPlatform('android')
+
+export const isAndroidAndWeb = () => !getPlatforms().some(r=>["ios","ipad","iphone","phablet","tablet","cordova","capacitor","electron","pwa" ,"mobile","mobileweb","hybrid"] .indexOf(r) >= 0)
 
 export const isMobile = () => isIos() || isAndroid()
 export const isWeb = () => !isMobile()
