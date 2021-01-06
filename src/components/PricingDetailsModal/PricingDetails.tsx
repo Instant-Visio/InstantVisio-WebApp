eeb19b7ecc94e18e9fc44fef0b8aca5ccd1417e2
Nicolas Hovart <hovart.nicolas@gmail.com>
2021-01-06 12:44:31 +0100

    Update PricingDetails.tsx

--- src/components/PricingDetailsModal/PricingDetails.tsx
+++ src/components/PricingDetailsModal/PricingDetails.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
+
+const ModalContent = styled.div`
+    margin: ${({ theme }) =>
+        `0rem ${theme.spacing.XXXL} 0rem ${theme.spacing.XXL}`};
+    h3 {
+        font-weight: bold;
+        margin-bottom: ${({ theme }) => theme.spacing.XXXL};
+    }
+    h3:nth-of-type(2) {
+        text-align: center;
+        margin-bottom: 0rem;
+    }
+    p {
+        margin-bottom: 0rem;
+    }
+    p:nth-of-type(2) {
+        margin-bottom: 2rem;
+    }
+`
 
 export default function PricingDetails() {
     const { t } = useTranslation('pricing-details')
 
     return (
-        <div>
+        <ModalContent>
             <h3>{t('information.packages')}</h3>
             <p>{t('information.1-credit')}</p>
             <p>{t('information.120mn')}</p>
@@ -15,6 +35,6 @@ export default function PricingDetails() {
                 alt="tableur_pricing"
                 width="100%"
             />
-        </div>
+        </ModalContent>
     )
 }
