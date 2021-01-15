ebced9e49e571c3f852fa6cb4a23edef92ef790f
Hugo Tembremande <htembremande@ineat.fr>
2021-01-15 12:29:30 +0100

    add firefox mobile as unsupported browser add custom message for iOS users not using safari mobile browser

--- src/pages/PremiumVideoCall/components/UnsupportedBrowserWarning/UnsupportedBrowserWarning.tsx
+++ src/pages/PremiumVideoCall/components/UnsupportedBrowserWarning/UnsupportedBrowserWarning.tsx
@@ -7,6 +7,8 @@ import React from 'react'
 import Video from 'twilio-video'
 import { Container, Link, Typography, Paper, Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
+import { isIOS, isMobile, isMobileSafari, isFirefox } from 'react-device-detect'
+
 
 const useStyles = makeStyles({
     container: {
@@ -20,10 +22,16 @@ const useStyles = makeStyles({
     },
 })
 
+const isMobileFirefox = () : boolean =>  isMobile && isFirefox
+
+const isIOSMobileSafari = () : boolean =>  isIOS && isMobileSafari
+
+const isVideoSupported = () : boolean => !Video.isSupported || !isMobileFirefox() || !isIOSMobileSafari()
+
 export default function ({ children }: { children: React.ReactElement }) {
     const classes = useStyles()
 
-    if (!Video.isSupported) {
+    if (!isVideoSupported) {
         return (
             <Container>
                 <Grid container justify="center" className={classes.container}>
@@ -34,26 +42,34 @@ export default function ({ children }: { children: React.ReactElement }) {
                                 className={classes.heading}>
                                 Browser or context not supported
                             </Typography>
-                            <Typography>
-                                Please open this application in one of the{' '}
-                                <Link
-                                    href="https://www.twilio.com/docs/video/javascript#supported-browsers"
-                                    target="_blank"
-                                    rel="noopener">
-                                    supported browsers
-                                </Link>
-                                .
-                                <br />
-                                If you are using a supported browser, please
-                                ensure that this app is served over a{' '}
-                                <Link
-                                    href="https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts"
-                                    target="_blank"
-                                    rel="noopener">
-                                    secure context
-                                </Link>{' '}
-                                (e.g. https or localhost).
-                            </Typography>
+                            {
+                                isIOS
+                                    ?
+                                    <Typography>
+                                        Please open this application in Safari or download the InstantVisio application.
+                                    </Typography>
+                                    :
+                                    <Typography>
+                                        Please open this application in one of the{' '}
+                                        <Link
+                                            href="https://www.twilio.com/docs/video/javascript#supported-browsers"
+                                            target="_blank"
+                                            rel="noopener">
+                                            supported browsers
+                                        </Link>
+                                        .
+                                        <br />
+                                        If you are using a supported browser, please
+                                        ensure that this app is served over a{' '}
+                                        <Link
+                                            href="https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts"
+                                            target="_blank"
+                                            rel="noopener">
+                                            secure context
+                                        </Link>{' '}
+                                        (e.g. https or localhost).
+                                    </Typography>
+                            }
                         </Paper>
                     </Grid>
                 </Grid>
