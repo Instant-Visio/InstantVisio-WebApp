76507c56a38212e88f61336aa336c4ae5a4d6930
ntchou <joe.lindien22@gmail.com>
2021-01-17 23:43:19 +0100

    [FRONT] fix: #599 ui on mobile on join visio group page - feature: #598 add translation on prejoin page

--- src/pages/PremiumVideoCall/components/PreJoinScreens/RoomNameScreen/RoomNameScreen.tsx
+++ src/pages/PremiumVideoCall/components/PreJoinScreens/RoomNameScreen/RoomNameScreen.tsx
@@ -13,6 +13,7 @@ import {
     InputLabel,
     Theme,
 } from '@material-ui/core'
+import { useTranslation } from 'react-i18next'
 import { useAppState } from '../../../state'
 import { useSelector } from 'react-redux'
 import { selectToken } from '../../../../../components/App/userSelector'
@@ -61,6 +62,7 @@ export default function RoomNameScreen({
     const { user } = useAppState()
     const token = useSelector(selectToken)
     const disableButtons = !name || !roomName || !token
+    const { t } = useTranslation('videocall')
 
     const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value)
@@ -78,19 +80,19 @@ export default function RoomNameScreen({
     return (
         <>
             <Typography variant="h5" className={classes.gutterBottom}>
-                Rejoindre une visio de groupe
+                {t('prejoin.heading')}
             </Typography>
             <Typography variant="body1">
                 {hasUsername
-                    ? "Enter the name of a room you'd like to join."
-                    : "Enter your name and the name of a room you'd like to join"}
+                    ? t('prejoin.room-name')
+                    : t('prejoin.username-and-room')}
             </Typography>
             <form onSubmit={handleSubmit}>
                 <div className={classes.inputContainer}>
                     {!hasUsername && (
                         <div className={classes.textFieldContainer}>
                             <InputLabel shrink htmlFor="input-user-name">
-                                Votre nom
+                                {t('prejoin.your-name')}
                             </InputLabel>
                             <TextField
                                 id="input-user-name"
